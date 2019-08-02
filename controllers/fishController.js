0df741dd874a1caec665e9dc78634c927a61b589
Zach Gordon <zachgordon@Zachs-MacBook-Pro.local>
2019-08-02 15:01:28 -0400

    added seed data to database

--- controllers/fishController.js
+++ controllers/fishController.js
@@ -8,4 +8,16 @@ fish.get('/', (req, res) => {
   res.render('index.ejs');
 });
 
+// SHOW
+
+
+// CALL SEED
+// fish.get('/seed/newfish/viaseedfile', (req, res) => {
+//   Fish.insertMany(fishSeed, (err, fish) => {
+//     if (err) { console.log(err); } else {
+//       res.send(fish);
+//     }
+//   });
+// });
+
 module.exports = fish;
