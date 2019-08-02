434617273f3eea2c89ce1cf959f14fa36ac6218b
Zach Gordon <zachgordon@Zachs-MacBook-Pro.local>
2019-08-02 15:11:06 -0400

    created landing page

--- server.js
+++ server.js
@@ -11,16 +11,18 @@ const fishController = require('./controllers/fishController.js');
 app.use(express.urlencoded({ extended: true }));
 app.use('/fish', fishController);
 
+// LANDING PAGE
+app.get('/', (req, res) => {
+  res.render('home.ejs');
+})
+
+
 // CONNECT TO MONGO
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/ZFH'
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true }, () => {
   console.log('connected to mongo database')
 });
 
-// CHECK TO SEE IF RUNNING
-app.get('/', (req, res) => {
-  res.send('app is running')
-});
 
 // EXPRESS LISTENER
 app.listen(PORT, () => console.log('Listening on port:', PORT));
