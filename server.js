4fd9145394466c08a7cfd6cbbda38e64292281b2
Zach Gordon <zachgordon@Zachs-MacBook-Pro.local>
2019-08-02 11:18:55 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,21 @@
+// DEPENDENCIES
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// DATABASE
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/ZFH'
+
+// CONNECT TO MONGO
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true }, () => {
+  console.log('connected to mongo database')
+});
+
+// CHECK TO SEE IF RUNNING
+app.get('/', (req, res) => {
+  res.send('app is running')
+});
+
+// EXPRESS LISTENER
+app.listen(PORT, () => console.log('Listening on port:', PORT));
