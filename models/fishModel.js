3dd9b42213abe8a3120978ec7209709b095da4c1
Zach Gordon <zachgordon@Zachs-MacBook-Pro.local>
2019-08-05 09:21:23 -0400

    added binomial name and moved some of the css to the public css page

--- models/fishModel.js
+++ models/fishModel.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const fishSchema = new mongoose.Schema({
   name: { type: String, required: true },
+  binomial: String,
   description: String,
   img: String,
   price: { type: Number, required: true, min: 0 },
