7a15f351a432b71087f879130dd43c0a919c00a5
Zach Gordon <zachgordon@Zachs-MacBook-Pro.local>
2019-08-06 16:42:10 -0400

    css and auth working

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(
     saveUninitialized: false
   }));
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(__dirname + 'public'));
+app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
