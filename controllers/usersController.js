d9275b4eb0e6ae1b3dc4fa86e3ae6cbbd6ebc5dd
Zach Gordon <zachgordon@Zachs-MacBook-Pro.local>
2019-08-08 14:42:46 -0400

    added first and last name to user controller and form

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -15,6 +15,10 @@ users.post('/', (req, res) => {
     res.render('./users/usersError.ejs');
   } else if (req.body.password === "") {
     res.render('./users/usersError.ejs');
+  } else if (req.body.firstname === "") {
+    res.render('./users/usersError.ejs');
+  } else if (req.body.lastname === "") {
+    res.render('./users/usersError.ejs');
   } else {
     req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
     if (req.body.isAdmin === "on") {
--- models/usersModel.js
+++ models/usersModel.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
+  firstname: { type: String, required: true },
+  lastname: { type: String, required: true },
   username: String,
   password: String,
   isAdmin: Boolean
