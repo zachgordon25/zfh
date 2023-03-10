31360c956e0d443239405d7a546f8a1b87379cf9
Zach Gordon <zachgordon25@gmail.com>
2023-03-10 14:54:48 -0500

    fixed bcrypt errors

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,8 +1,7 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const users = express.Router();
 const User = require('../models/usersModel.js');
-
 
 // NEW (CLIENT)
 users.get('/register', (req, res) => {
@@ -12,18 +11,15 @@ users.get('/register', (req, res) => {
 // ENCRYPTING PASSWORD
 users.post('/', (req, res) => {
   req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
-  if (req.body.isAdmin === "on") {
+  if (req.body.isAdmin === 'on') {
     req.body.isAdmin = true;
   } else {
     req.body.isAdmin = false;
   }
   User.create(req.body, (err, createdUser) => {
-    res.redirect('/fish');;
+    res.redirect('/fish');
   });
 });
-;
-
-
 // CREATE (SERVER)
 users.post('/', (req, res) => {
   User.create(req.body, (err, createdUser) => {
